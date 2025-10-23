d3363f5dfaa67fa235f43c7e9bc14af5d98da065
ppnggn <timurwdo@gmail.com>
2025-10-24 01:00:38 +0700

    auth pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:drazzle/core/router/app_router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,10 +8,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-    );
+    return MaterialApp.router(routerConfig: router);
   }
 }
